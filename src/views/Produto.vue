922c51e46057c6c9bf99bde9e7e2992c6068d3b8
Vinicius Araujo <56658014+vabarboza@users.noreply.github.com>
2021-09-17 21:58:41 -0300

    Produto

---
+++ src/views/Produto.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="produto">
+    <div class="container">
+      <h4>Cadastro de Produtos</h4>
+
+      <form class="row g-3">
+        <div class="col-md-4">
+          <label for="validationDefault01" class="form-label"
+            >Nome do Produto</label
+          >
+          <input type="text" class="form-control" required />
+        </div>
+        <div class="col-md-4">
+          <label for="validationDefault02" class="form-label">Quantidade</label>
+          <input type="number" class="form-control" required />
+        </div>
+        <div class="col-md-4">
+          <label for="validationDefault02" class="form-label">Valor</label>
+          <input type="text" class="form-control" required />
+        </div>
+        <div class="col-md-12">
+          <label for="validationDefault03" class="form-label"
+            >Descrição do Produto</label
+          >
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            required
+          ></textarea>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-outline-success" type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
